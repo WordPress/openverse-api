633e9891f305d0fea5472042db8851029a018179
Dhruv Bhanushali <dhruv_b@live.com>
2021-10-14 12:00:53 +0400

    Separate the healthcheck view from the OAuth2 views

--- openverse_api/catalog/api/views/oauth2_views.py
+++ openverse_api/catalog/api/views/oauth2_views.py
@@ -30,21 +30,6 @@ from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 
-class HealthCheck(APIView):
-    """
-    Returns a `200 OK` response if the server is running.
-
-    This endpoint is used in production to ensure that the server should receive
-    traffic. If no response is provided, the server is deregistered from the
-    load balancer and destroyed.
-    """
-
-    swagger_schema = None
-
-    def get(self, request, format=None):
-        return Response({"status": "200 OK"}, status=200)
-
-
 class Register(APIView):
     swagger_schema = CustomAutoSchema
     register_api_oauth2_description = """
