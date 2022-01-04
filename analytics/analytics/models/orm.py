9085252970b449157e0fc8be3bcd51e81c02723b
Dhruv Bhanushali <dhruv_b@live.com>
2022-01-04 09:27:22 +0400

    Extra `MediaMixin` from `Image` and use to define `Audio`

--- analytics/analytics/models/orm.py
+++ analytics/analytics/models/orm.py
@@ -11,13 +11,21 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class Image(Base):
-    """Managed by Django model ``Image``"""
-
-    __tablename__ = "image"
-
+class MediaMixin:
     id = Column(Integer, primary_key=True)
     identifier = Column(UUID)
     source = Column(String)
     provider = Column(String)
     title = Column(String)
+
+
+class Image(Base, MediaMixin):
+    """Managed by Django model ``Image``"""
+
+    __tablename__ = "image"
+
+
+class Audio(Base, MediaMixin):
+    """Managed by Django model ``Audio``"""
+
+    __tablename__ = "audio"
