dcacfdb3b82db9a4a953ac32f1a0bbc1f3b2c75c
Dhruv Bhanushali <dhruv_b@live.com>
2022-01-22 10:46:18 +0400

    Align `CREATE VIEW` with mock schemas

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -84,15 +84,15 @@ docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy 
 	CREATE VIEW audioset_view
 	AS
 		SELECT DISTINCT
-			cast(audio_set ->> 'foreign_identifier'  as varchar(1000)) as foreign_identifier,
-			cast(audio_set ->> 'title'               as varchar(2000)) as title,
-			cast(audio_set ->> 'foreign_landing_url' as varchar(1000)) as foreign_landing_url,
-			cast(audio_set ->> 'creator'             as varchar(2000)) as creator,
-			cast(audio_set ->> 'creator_url'         as varchar(2000)) as creator_url,
-			cast(audio_set ->> 'url'                 as varchar(1000)) as url,
-			cast(audio_set ->> 'filesize'            as integer)       as filesize,
-			cast(audio_set ->> 'filetype'            as varchar(80))   as filetype,
-			cast(audio_set ->> 'thumbnail'           as varchar(1000)) as thumbnail,
+			((audio_view.audio_set ->> 'foreign_identifier'  :: text)) :: character varying(1000) as foreign_identifier,
+			((audio_view.audio_set ->> 'title'               :: text)) :: character varying(2000) as title,
+			((audio_view.audio_set ->> 'foreign_landing_url' :: text)) :: character varying(1000) as foreign_landing_url,
+			((audio_view.audio_set ->> 'creator'             :: text)) :: character varying(2000) as creator,
+			((audio_view.audio_set ->> 'creator_url'         :: text)) :: character varying(2000) as creator_url,
+			((audio_view.audio_set ->> 'url'                 :: text)) :: character varying(1000) as url,
+			((audio_view.audio_set ->> 'filesize'            :: text)) :: integer                 as filesize,
+			((audio_view.audio_set ->> 'filetype'            :: text)) :: character varying(80)   as filetype,
+			((audio_view.audio_set ->> 'thumbnail'           :: text)) :: character varying(1000) as thumbnail,
 			provider
 		FROM audio_view
 		WHERE audio_set IS NOT NULL;
