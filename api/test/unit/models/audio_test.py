0abcad7a6b5f05bac53ef124b9c0ad68dccdb0f8
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-22 18:20:31 +0400

    Update failing test

--- api/test/unit/models/audio_test.py
+++ api/test/unit/models/audio_test.py
@@ -1,5 +1,6 @@
 import uuid
 from unittest import mock
+from unittest.mock import Mock
 
 import pytest
 from catalog.api.models.audio import Audio, AudioAddOn
@@ -18,10 +19,11 @@ def audio_fixture():
 
 
 @pytest.mark.django_db
-@mock.patch("catalog.api.models.audio.generate_peaks")
-def test_audio_waveform_caches(generate_peaks_mock, audio_fixture):
+@mock.patch("requests.post")
+def test_audio_waveform_caches(post, audio_fixture):
     mock_waveform = [0.4, 0.3, 0.1, 0, 1, 0.6]
-    generate_peaks_mock.return_value = mock_waveform
+    awf_res = {"peak_sets": {"1000": {"length": 6, "peaks": mock_waveform}}}
+    post.return_value = Mock(json=Mock(return_value=awf_res))
 
     assert AudioAddOn.objects.count() == 0
     assert audio_fixture.get_or_create_waveform() == mock_waveform
@@ -33,7 +35,7 @@ def test_audio_waveform_caches(generate_peaks_mock, audio_fixture):
     )
     assert audio_fixture.get_or_create_waveform() == mock_waveform
     # Should only be called once if Audio.get_or_create_waveform is using the DB value on subsequent calls
-    generate_peaks_mock.assert_called_once()
+    post.assert_called_once()
 
     # Ensure there are no foreign constraints on the AudioAddOn that would cause failures during refresh
     audio_fixture.delete()
