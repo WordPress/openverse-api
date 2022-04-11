d31a55c34ba472a94008190d4961fa8ecee47b35
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-04-11 08:52:18 +0200

    Fix formatting

--- api/test/run_test.sh
+++ api/test/run_test.sh
@@ -16,9 +16,9 @@ pytest -s --disable-pytest-warnings $TEST_ARG
 
 succeeded=$?
 if [[ $succeeded -eq 0 ]]; then
-	printf "${green}:-) All tests passed${endcol}\n"
+    printf "${green}:-) All tests passed${endcol}\n"
 else
     printf "\n\n${red}:'( Some tests did not pass${endcol}\n"
-    printf "Hint: \`just logs [service]\` will print the docker logs and may be helpful for debugging.\n\n"
+    printf "Hint: \`just logs [service]\` will print the Docker logs and may be helpful for debugging.\n\n"
 fi
 exit $succeeded
