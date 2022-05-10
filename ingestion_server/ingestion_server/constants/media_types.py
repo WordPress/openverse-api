f954b660c4314ea2e4ae02252493c9b5c301b510
foroveralls <jeverallp@gmail.com>
2022-05-10 11:26:20 +0200

    3d Model type added to media_types.py

--- ingestion_server/ingestion_server/constants/media_types.py
+++ ingestion_server/ingestion_server/constants/media_types.py
@@ -4,5 +4,6 @@ Also see `api/constants/media_types.py`
 
 AUDIO_TYPE = "audio"
 IMAGE_TYPE = "image"
+3D_MODEL_TYPE = "model_3d"
 
-MEDIA_TYPES = [AUDIO_TYPE, IMAGE_TYPE]
+MEDIA_TYPES = [AUDIO_TYPE, IMAGE_TYPE, 3D_MODEL_TYPE]
