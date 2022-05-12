aa10f65d4e0350b78bb2da20bdcf95020677277c
Dhruv Bhanushali <dhruv_b@live.com>
2022-05-12 11:57:13 +0400

    Create new field to automatically validate enums

--- api/catalog/api/serializers/base.py
+++ api/catalog/api/serializers/base.py
@@ -3,6 +3,8 @@ import re
 from django.conf import settings
 from rest_framework import serializers
 
+from catalog.api.utils.help_text import make_comma_separated_help_text
+
 
 class SchemableHyperlinkedIdentityField(serializers.HyperlinkedIdentityField):
     """
@@ -22,3 +24,40 @@ class SchemableHyperlinkedIdentityField(serializers.HyperlinkedIdentityField):
             url = re.sub(r"^\w+://", f"{self.scheme}://", url, 1)
 
         return url
+
+
+class EnumCharField(serializers.CharField):
+    """
+    This field extends the ``CharField`` to add enum validation.
+    """
+
+    default_error_messages = serializers.CharField.default_error_messages | {
+        "outside_enum": "Invalid value: {given}. Allowed values: {allowed}"
+    }
+
+    def __init__(self, plural: str, enum_var: set[str], **kwargs):
+        kwargs["help_text"] = make_comma_separated_help_text(enum_var, plural)
+        super().__init__(**kwargs)
+
+        self.enum_var = enum_var
+
+    def _validate_enum(self, given_value: str):
+        """
+        Validate whether the given values are all members of the given enum.
+
+        :param given_value: the comma separated list received in the input
+        :return: the lower cased form of the input, if the input is valid
+        :raise: ``ValidationError``, if the input is invalid
+        """
+
+        lower = given_value.lower()
+        input_values = lower.split(",")
+        for value in input_values:
+            if value not in self.enum_var:
+                self.fail("outside_enum", given=value, allowed=self.enum_var)
+        return lower
+
+    def to_internal_value(self, data):
+        data = super().to_internal_value(data)
+        self._validate_enum(data)
+        return data
