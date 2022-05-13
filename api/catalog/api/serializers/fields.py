2bd2a0aad0ddb013ee00e2716fb885db06b5e069
Dhruv Bhanushali <hi@dhruvkb.dev>
2022-05-13 13:30:51 +0400

    Remove stray character
    Co-authored-by: Olga Bulat <obulat@gmail.com>

--- api/catalog/api/serializers/fields.py
+++ api/catalog/api/serializers/fields.py
@@ -28,7 +28,7 @@ class SchemableHyperlinkedIdentityField(serializers.HyperlinkedIdentityField):
 
 class EnumCharField(serializers.CharField):
     """
-    This field extends the ``CharField`` to add enum validation.¡
+    This field extends the ``CharField`` to add enum validation.
     """
 
     default_error_messages = serializers.CharField.default_error_messages | {
