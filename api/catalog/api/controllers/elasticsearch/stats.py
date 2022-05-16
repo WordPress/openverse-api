56118da123bc072755a2fd22d6c9babdee3c5517
Dhruv Bhanushali <dhruv_b@live.com>
2022-05-16 17:47:56 +0400

    Dedent code and set cache only on success

--- api/catalog/api/controllers/elasticsearch/stats.py
+++ api/catalog/api/controllers/elasticsearch/stats.py
@@ -7,45 +7,44 @@ from elasticsearch.exceptions import NotFoundError
 from elasticsearch_dsl import Search
 
 
-SOURCE_CACHE_TIMEOUT = 60 * 20
+SOURCE_CACHE_TIMEOUT = 60 * 20  # seconds
 
 
 def get_stats(index: Literal["image", "audio"]):
     """
-    Given an index, find all available data sources and return their counts.
+    Given an index, find all available data sources and return their counts. This data
+    is cached in Redis. See ``load_sample_data.sh`` for example of clearing the cache.
 
     :param index: the Elasticsearch index name
     :return: a dictionary mapping sources to the count of their media items
     """
 
     source_cache_name = "sources-" + index
-    cache_fetch_failed = False
     try:
         sources = cache.get(key=source_cache_name)
+        return sources
     except ValueError:
-        cache_fetch_failed = True
-        sources = None
-        log.warning("Source cache fetch failed due to corruption")
-    if type(sources) == list or cache_fetch_failed:
-        # Invalidate old provider format.
-        cache.delete(key=source_cache_name)
-    if not sources:
-        # Don't increase `size` without reading this issue first:
-        # https://github.com/elastic/elasticsearch/issues/18838
-        size = 100
-        try:
-            s = Search(using="default", index=index)
-            s.aggs.bucket(
-                "unique_sources",
-                "terms",
-                field="source.keyword",
-                size=size,
-                order={"_key": "desc"},
-            )
-            results = s.execute()
-            buckets = results["aggregations"]["unique_sources"]["buckets"]
-        except NotFoundError:
-            buckets = [{"key": "none_found", "doc_count": 0}]
+        log.warning("Source cache fetch failed")
+
+    # Don't increase `size` without reading this issue first:
+    # https://github.com/elastic/elasticsearch/issues/18838
+    size = 100
+    try:
+        s = Search(using="default", index=index)
+        s.aggs.bucket(
+            "unique_sources",
+            "terms",
+            field="source.keyword",
+            size=size,
+            order={"_key": "desc"},
+        )
+        results = s.execute()
+        buckets = results["aggregations"]["unique_sources"]["buckets"]
         sources = {result["key"]: result["doc_count"] for result in buckets}
+    except NotFoundError:
+        sources = {}
+
+    if sources:
         cache.set(key=source_cache_name, timeout=SOURCE_CACHE_TIMEOUT, value=sources)
+
     return sources
