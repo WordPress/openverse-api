282060f4431f079a64ec5f96d9e7467742213c58
Dhruv Bhanushali <dhruv_b@live.com>
2022-05-19 12:05:19 +0400

    Break `if`-`else` across multiple lines

--- ingestion_server/test/run_test.sh
+++ ingestion_server/test/run_test.sh
@@ -8,7 +8,11 @@ endcol="\e[0m"
 # ```
 # $	./test/run_test.sh test/unit_test.py
 # ```
-if [ $# -ge 1 ]; then TEST_ARG="$@"; else TEST_ARG="test/"; fi
+if [ $# -ge 1 ]; then
+	TEST_ARG="$@"
+else
+	TEST_ARG="test/"
+fi
 
 PYTHONWARNINGS="ignore:Unverified HTTPS request" \
 PYTHONPATH=. \
