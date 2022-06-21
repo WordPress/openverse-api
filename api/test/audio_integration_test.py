3ba83e441e284cec74fbf813805da5b2f42e39fa
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2022-06-21 17:59:37 -0400

    Apply review suggestions

--- api/test/audio_integration_test.py
+++ api/test/audio_integration_test.py
@@ -90,7 +90,7 @@ def test_audio_detail_without_thumb():
 
 def test_audio_search_without_thumb():
     """The first audio of this search should not have a thumbnail."""
-    resp = requests.get(f"{API_URL}/v1/audio?q=zaus&filter_dead=false")
+    resp = requests.get(f"{API_URL}/v1/audio/?q=zaus&filter_dead=false")
     assert resp.status_code == 200
     parsed = json.loads(resp.text)
     assert parsed["results"][0]["thumbnail"] is None
