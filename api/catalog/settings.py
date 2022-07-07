a408ad2bb7633bd2e6bf1b8033a72b0e026947c0
Raghuvar arora <raghuvararora@gmail.com>
2022-07-08 00:35:50 +0530

    made token expiry time configurable

--- api/catalog/settings.py
+++ api/catalog/settings.py
@@ -114,7 +114,9 @@ OAUTH2_PROVIDER = {
         "read": "Read scope",
         "write": "Write scope",
     },
-    "ACCESS_TOKEN_EXPIRE_SECONDS": 3600 * 12,
+    "ACCESS_TOKEN_EXPIRE_SECONDS": config(
+        "ACCESS_TOKEN_EXPIRE_SECONDS", default=3600 * 12, cast=int
+    ),
 }
 
 OAUTH2_PROVIDER_APPLICATION_MODEL = "api.ThrottledApplication"
