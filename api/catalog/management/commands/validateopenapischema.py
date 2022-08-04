615488d85a79b9af0cd92f8720e7de9763f12dac
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-08-04 16:54:32 -0400

    Use drf-yasg schema instead of DRF native
    DRF is not currently correctly configured to generate a schema with our setup. Under the current settings it causes invalid operation IDs to be generated (duplicates). For now we can validate against the v2 schema and open an issue to configure the DRF native schema generation in the future.

--- api/catalog/management/commands/validateopenapischema.py
+++ api/catalog/management/commands/validateopenapischema.py
@@ -1,15 +1,24 @@
 from django.core.management import call_command, BaseCommand
-from openapi_spec_validator import validate_spec
+from openapi_spec_validator import validate_v2_spec
 from openapi_spec_validator.readers import read_from_filename
 from pathlib import Path
 import os
+from tempfile import gettempdir
+from argparse import ArgumentParser
 
 class Command(BaseCommand):
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument(
+            "--output-dir",
+            default=Path(".").absolute(),
+            help="The direction into which to output the spec file. Defaults the the current working directory."
+        )
+
     def handle_validation(self, spec_dict: dict):
         """
         This method should raise an error if the spec is not valid.
         """
-        validate_spec(spec_dict)
+        validate_v2_spec(spec_dict)
 
         invalid_paths = [p for p in spec_dict["paths"].keys() if not p.endswith('/')]
 
@@ -19,12 +28,9 @@ class Command(BaseCommand):
             f"slash: {', '.join(invalid_paths)}"
         )
 
-
-    def handle(self, *args, **kwargs):
-        file_path = str((Path(__file__).parent / "openapi.yaml").absolute())
-        call_command('generateschema', file=file_path)
+    def handle(self, *args, **options):
+        file_path = str((Path(options["output_dir"]).absolute() / "openapi.yaml").absolute())
+        call_command("generate_swagger", file_path, overwrite=True)
         spec_dict, spec_url = read_from_filename(file_path)
-        try:
-            self.handle_validation(spec_dict)
-        finally:
-            os.remove(file_path)
+        self.handle_validation(spec_dict)
+        os.remove(file_path)
