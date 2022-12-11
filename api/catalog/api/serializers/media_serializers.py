50c4165fb9e7cf0b1886940c43acedc3c2f181ec
sahil-R <63741193+sahil-R@users.noreply.github.com>
2022-12-11 16:28:37 +0530

    delete Pipfile and Pipfile.lock from root directory,changed default accuracy of tags to None

--- api/catalog/api/serializers/media_serializers.py
+++ api/catalog/api/serializers/media_serializers.py
@@ -277,7 +277,7 @@ class TagSerializer(serializers.Serializer):
         help_text="The name of a detailed tag.",
     )
     accuracy = serializers.FloatField(
-        default="0.0",
+        default=None,
         help_text="The accuracy of a machine-generated tag. Human-generated "
         "tags do not have an accuracy field.",
     )
